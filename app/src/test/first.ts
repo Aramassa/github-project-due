4a1210e1d70c48d063033bbaba7bd5b554710075
Aramassa <aramassa31@gmail.com>
2020-02-29 08:52:06 +0900

    test を should に変更

---
+++ app/src/test/first.ts
@@ -0,0 +1,9 @@
+import { should } from 'chai';
+should()
+
+describe("test", function(){
+
+    it('should return -1 when the value is not present', function() {
+        true.should.eq(true )
+    });
+});
