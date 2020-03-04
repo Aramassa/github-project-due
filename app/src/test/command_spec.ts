295bf027b07e14652f4bfb062b54887c1fab4ae9
Aramassa <aramassa31@gmail.com>
2020-03-04 23:52:36 +0900

    適切な await を設定

--- app/src/test/command_spec.ts
+++ app/src/test/command_spec.ts
@@ -7,22 +7,22 @@ import {Project} from "../lib/due/Project";
 describe("Vorpal Command", function() {
     it('show repo info', async function(){
         this.timeout(5000);
-        VorpalCommand.cmdRepoInfo();
+        await VorpalCommand.cmdRepoInfo();
     });
 
     it('list project', async function(){
         this.timeout(5000);
-        VorpalCommand.cmdListProject();
+        await VorpalCommand.cmdListProject();
     });
 
     it('set project id', async function(){
         this.timeout(5000);
-        VorpalCommand.cmdSetCurrentProject({id: 12345});
+        await VorpalCommand.cmdSetCurrentProject({id: 12345});
     })
 
     it('list tasks on project', async function(){
         this.timeout(5000);
-        VorpalCommand.cmdSetCurrentProject({id: 4038195})
+        await VorpalCommand.cmdSetCurrentProject({id: 4038195})
         let proj:Project = await VorpalCommand.cmdListIssues();
 
         proj.debug_line.should.contains("Test Project 001");
